b9880259e8886b7b588614d02c527c6c37fbe5ff
Jaime Jesús Martínez Terrasa <jmterrasa@icloud.com>
2024-03-05 21:47:43 +0100

    structure modified

---
+++ TriForkApp/Views/OrganizationView.swift
@@ -0,0 +1,18 @@
+//
+//  OrganizationView.swift
+//  TriForkTestApp
+//
+//  Created by Jaime Jesús Martínez Terrasa on 5/3/24.
+//
+
+import SwiftUI
+
+struct OrganizationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrganizationView()
+}
---
+++ TriForkApp/Views/RepositoryView.swift
@@ -0,0 +1,18 @@
+//
+//  RepositoryView.swift
+//  TriForkTestApp
+//
+//  Created by Jaime Jesús Martínez Terrasa on 5/3/24.
+//
+
+import SwiftUI
+
+struct RepositoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RepositoryView()
+}
